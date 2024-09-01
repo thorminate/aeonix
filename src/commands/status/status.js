87fd175e905612963f75fe5d5fb8332e3a066a85
Thorminate <thorwaag11@gmail.com>
2024-09-01 12:59:30 +0200

    phixeroo3?

--- src/commands/status/status.js
+++ src/commands/status/status.js
@@ -211,6 +211,7 @@ module.exports = {
             await buttonInteraction.reply({
               content: `loading...`,
               components: updatedComponents,
+              ephemeral: true,
             });
             // Define buttons for submenu
 
