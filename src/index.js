9c2ac790aff28da5c81daa30ae1a06b4567c5041
thor <thorwaag11@gmail.com>
2024-09-07 19:47:43 +0200

    Sharding added (for some reason)

--- src/index.js
+++ src/index.js
@@ -1,48 +1,9 @@
-// Define Requirements
-require("dotenv").config();
-const { Client, IntentsBitField } = require("discord.js");
-const mongoose = require("mongoose");
-const eventHandler = require("./handlers/eventHandler");
+const { ShardingManager } = require("discord.js");
 
-// Define 'bot'
-const bot = new Client({
-  intents: [
-    IntentsBitField.Flags.Guilds,
-    IntentsBitField.Flags.GuildModeration,
-    IntentsBitField.Flags.GuildEmojisAndStickers,
-    IntentsBitField.Flags.GuildIntegrations,
-    IntentsBitField.Flags.GuildWebhooks,
-    IntentsBitField.Flags.GuildInvites,
-    IntentsBitField.Flags.GuildVoiceStates,
-    IntentsBitField.Flags.GuildMessageReactions,
-    IntentsBitField.Flags.GuildMessageTyping,
-    IntentsBitField.Flags.DirectMessages,
-    IntentsBitField.Flags.DirectMessageReactions,
-    IntentsBitField.Flags.DirectMessageTyping,
-    IntentsBitField.Flags.GuildScheduledEvents,
-    IntentsBitField.Flags.AutoModerationConfiguration,
-    IntentsBitField.Flags.AutoModerationExecution,
-    IntentsBitField.Flags.GuildMessagePolls,
-    IntentsBitField.Flags.DirectMessagePolls,
-    IntentsBitField.Flags.GuildMembers,
-    IntentsBitField.Flags.GuildMessages,
-    IntentsBitField.Flags.MessageContent,
-    IntentsBitField.Flags.GuildPresences,
-  ],
+const manager = new ShardingManager("./src/bot.js", {
+  token: process.env.TOKEN,
 });
 
-const MongoDBToken = process.env.MONGODB_URI + "/the_system";
-const DiscordToken = process.env.TOKEN;
-// Connect to DB and Discord.
-(async () => {
-  try {
-    await mongoose.connect(MongoDBToken);
-    console.log("Connected to DB.");
+manager.on("shardCreate", (shard) => console.log(`Launched shard ${shard.id}`));
 
-    eventHandler(bot);
-
-    bot.login(DiscordToken);
-  } catch (error) {
-    console.log(`Index Error: ${error}`);
-  }
-})();
+manager.spawn();
