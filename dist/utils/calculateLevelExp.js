72321ff62ede70a951cc779b508d0163997700f6
Thorminate <thorwaag11@gmail.com>
2024-10-09 16:08:20 +0200

    Delete dist directory

--- dist/utils/calculateLevelExp.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// exports the required exp to level up for a given level
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (level) => {
-    // Export the function.
-    const output = level ** 2 || 1; // Calculate the output.
-    const roundedOutput = Math.round(output); // Round the output.
-    return roundedOutput; // Return the rounded output.
-};
